5ea7d86c43d2a044f19e8cb186a551fc355b9c09
azu <azuciao@gmail.com>
2021-09-23 23:35:14 +0900

    update create-draft

--- actions/create-draft/src/index.ts
+++ actions/create-draft/src/index.ts
@@ -3,20 +3,20 @@ import { Octokit } from "@octokit/rest";
 import { UrlWithStringQuery } from "url";
 
 export type Issue = {
-  title: string;
-  body: string;
-  url: string;
-  number: number;
+    title: string;
+    body: string;
+    url: string;
+    number: number;
 }
 export const fetchIssues = async (
-  options: { owner: string; repo: string; labels: string[]; GITHUB_TOKEN: string; },
+    options: { owner: string; repo: string; labels: string[]; GITHUB_TOKEN: string; },
 ): Promise<Issue[]> => {
-  const graphqlWithAuth = graphql.defaults({
-    headers: {
-      authorization: `token ${options.GITHUB_TOKEN}`
-    }
-  });
-  const QUERY = `query ($owner: String!, $repo: String!, $labels: [String]) {
+    const graphqlWithAuth = graphql.defaults({
+        headers: {
+            authorization: `token ${options.GITHUB_TOKEN}`
+        }
+    });
+    const QUERY = `query ($owner: String!, $repo: String!, $labels: [String]) {
   repository(owner: $owner, name: $repo) {
     issues(first: 20, filterBy: {labels: $labels}) {
       nodes {
@@ -30,27 +30,26 @@ export const fetchIssues = async (
 }
 
 `;
-  const { repository } = await graphqlWithAuth<{
-    repository: {
-      issues: {
-        nodes: Issue[]
-      }
-    };
-  }>(QUERY, { owner: options.owner, repo: options.repo, labels: options.labels });
-  return repository.issues.nodes;
+    const { repository } = await graphqlWithAuth<{
+        repository: {
+            issues: {
+                nodes: Issue[]
+            }
+        };
+    }>(QUERY, { owner: options.owner, repo: options.repo, labels: options.labels });
+    return repository.issues.nodes;
 };
 
 
-
 export const createNextTagVersion = async (
-  options: { owner: string; repo: string; GITHUB_TOKEN: string; },
+    options: { owner: string; repo: string; GITHUB_TOKEN: string; },
 ): Promise<{ tagName: string; type: "new" | "update", releaseId?: number; }> => {
-  const graphqlWithAuth = graphql.defaults({
-    headers: {
-      authorization: `token ${options.GITHUB_TOKEN}`
-    }
-  });
-  const QUERY = `query ($owner: String!, $repo: String!) {
+    const graphqlWithAuth = graphql.defaults({
+        headers: {
+            authorization: `token ${options.GITHUB_TOKEN}`
+        }
+    });
+    const QUERY = `query ($owner: String!, $repo: String!) {
   repository(owner: $owner, name: $repo) {
     releases(first: 1) {
       nodes {
@@ -67,126 +66,142 @@ export const createNextTagVersion = async (
 }
 
 `;
-  const { repository } = await graphqlWithAuth<{
-    repository: {
-      releases: {
-        nodes: {
-          databaseId: number;
-          tagName: string;
-          isDraft: boolean;
-        }[]
-        totalCount: number;
-      },
-      latestRelease: {
-        databaseId: number,
-        isDraft: boolean,
-        tagName: string;
-      } | null
-    };
-  }>(QUERY, { owner: options.owner, repo: options.repo });
-  const initialVersion = 1;
-  const currentVersion = repository.latestRelease ? parseInt(repository.latestRelease.tagName, 10) : initialVersion;
-  // first release, but has draf
-  const latestDraft = (() => {
-    const release = repository.releases.nodes[0];
-    if (!release) {
-      return;
+    const { repository } = await graphqlWithAuth<{
+        repository: {
+            releases: {
+                nodes: {
+                    databaseId: number;
+                    tagName: string;
+                    isDraft: boolean;
+                }[]
+                totalCount: number;
+            },
+            latestRelease: {
+                databaseId: number,
+                isDraft: boolean,
+                tagName: string;
+            } | null
+        };
+    }>(QUERY, { owner: options.owner, repo: options.repo });
+    const initialVersion = 1;
+    const currentVersion = repository.latestRelease ? parseInt(repository.latestRelease.tagName, 10) : initialVersion;
+    // first release, but has draf
+    const latestDraft = (() => {
+        const release = repository.releases.nodes[0];
+        if (!release) {
+            return;
+        }
+        if (release.isDraft) {
+            return release
+        }
+        return;
+    })();
+    if (latestDraft) {
+        return {
+            type: "update",
+            tagName: latestDraft.tagName,
+            releaseId: latestDraft.databaseId
+        }
     }
-    if (release.isDraft) {
-      return release
+    // first release
+    if (repository.latestRelease === null) {
+        return {
+            tagName: `v${currentVersion}`,
+            type: "new"
+        }
     }
-    return;
-  })();
-  if (latestDraft) {
-    return {
-      type: "update",
-      tagName: latestDraft.tagName,
-      releaseId: latestDraft.databaseId
+    // v1, v2 ...
+    if (currentVersion === repository.releases.totalCount) {
+        return {
+            tagName: `v${currentVersion + 1}`,
+            type: "new"
+        }
     }
-  }
-  // first release
-  if (repository.latestRelease === null) {
-    return {
-      tagName: `v${currentVersion}`,
-      type: "new"
-    }
-  }
-  // v1, v2 ...
-  if (currentVersion === repository.releases.totalCount) {
-    return {
-      tagName: `v${currentVersion + 1}`,
-      type: "new"
-    }
-  }
+    throw new Error("Unknown response:" + repository);
 };
 
 const template = (issues: Issue[]) => {
-  return issues.map(issue => {
-    return `# ${issue.title} #${issue.number}
+    return issues.map(issue => {
+        return `# ${issue.title} #${issue.number}
 
 ${issue.body}    
 `
-  }).join("\n\n");
+    }).join("\n\n");
 }
-async function createDraftRelease({ type, releaseId, owner, repo, releaseName, tagName, releaseBody, commitSha, GITHUB_TOKEN }:
-  { type: "new" | "update", releaseId?: number; owner: string; repo: string; releaseName: string; releaseBody: string; tagName: string, commitSha: string; GITHUB_TOKEN: string }) {
-  const octokit = new Octokit({
-    auth: GITHUB_TOKEN
-  });
-  // Create a release
-  // API Documentation: https://developer.github.com/v3/repos/releases/#create-a-release
-  // Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-create-release
-  if (type === "new") {
-    await octokit.repos.createRelease({
-      owner,
-      repo,
-      tag_name: tagName,
-      name: releaseName,
-      body: releaseBody,
-      draft: true,
-      prerelease: false,
-      target_commitish: commitSha
+
+async function createDraftRelease({
+                                      type,
+                                      releaseId,
+                                      owner,
+                                      repo,
+                                      releaseName,
+                                      tagName,
+                                      releaseBody,
+                                      commitSha,
+                                      GITHUB_TOKEN
+                                  }:
+                                      { type: "new" | "update", releaseId?: number; owner: string; repo: string; releaseName: string; releaseBody: string; tagName: string, commitSha: string; GITHUB_TOKEN: string }) {
+    const octokit = new Octokit({
+        auth: GITHUB_TOKEN
     });
-  } else if (type === "update" && releaseId) {
-    await octokit.repos.updateRelease({
-      release_id: releaseId,
-      owner,
-      repo,
-      tag_name: tagName,
-      name: releaseName,
-      body: releaseBody,
-      draft: true,
-      prerelease: false,
-      target_commitish: commitSha
-    });
-  }
+    // Create a release
+    // API Documentation: https://developer.github.com/v3/repos/releases/#create-a-release
+    // Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-create-release
+    if (type === "new") {
+        await octokit.repos.createRelease({
+            owner,
+            repo,
+            tag_name: tagName,
+            name: releaseName,
+            body: releaseBody,
+            draft: true,
+            prerelease: false,
+            target_commitish: commitSha
+        });
+    } else if (type === "update" && releaseId) {
+        await octokit.repos.updateRelease({
+            release_id: releaseId,
+            owner,
+            repo,
+            tag_name: tagName,
+            name: releaseName,
+            body: releaseBody,
+            draft: true,
+            prerelease: false,
+            target_commitish: commitSha
+        });
+    }
 }
 
 if (require.main) {
-  fetchIssues({
-    GITHUB_TOKEN: process.env.GITHUB_TOKEN!,
-    labels: ["Status: Post"],
-    owner: "azu",
-    repo: "book-review"
-  }).then(async res => {
-    const next = await createNextTagVersion({
-      GITHUB_TOKEN: process.env.GITHUB_TOKEN!,
-      owner: "azu",
-      repo: "book-review",
-    });
-    console.log("next", next);
-    const body = template(res);
-    const title = "Draft";
-    createDraftRelease({
-      owner: "azu",
-      repo: "book-review",
-      GITHUB_TOKEN: process.env.GITHUB_TOKEN!,
-      commitSha: process.env.GIT_COMMIT_SHA!,
-      type: next.type,
-      tagName: next.tagName,
-      releaseId: next.releaseId,
-      releaseName: title,
-      releaseBody: body
+    const GITHUB_REPOSITORY = process.env.GITHUB_REPOSITORY!
+    const DRAFT_LABEL = process.env.DRAFT_LABEL!
+    const GITHUB_TOKEN = process.env.GITHUB_TOKEN!;
+    const GIT_COMMIT_SHA = process.env.GIT_COMMIT_SHA!;
+    const [owner, repo] = GITHUB_REPOSITORY.split("/");
+    fetchIssues({
+        GITHUB_TOKEN,
+        labels: [DRAFT_LABEL],
+        owner,
+        repo
+    }).then(async res => {
+        const next = await createNextTagVersion({
+            GITHUB_TOKEN,
+            owner,
+            repo,
+        });
+        const body = template(res);
+        const title = "Draft";
+        await createDraftRelease({
+            owner,
+            repo,
+            GITHUB_TOKEN,
+            commitSha: GIT_COMMIT_SHA,
+            type: next.type,
+            tagName: next.tagName,
+            releaseId: next.releaseId,
+            releaseName: title,
+            releaseBody: body
+        })
     })
-  })
 }
